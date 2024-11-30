78c90b953dd90c40096627121ec50ea65b9e2f12
Nelgie <nelgie.ferrol94@gmail.com>
2024-11-30 20:43:06 +0100

    avancement tp1

--- src/main/java/com/tp1/core/controleur/PassagerControleur.java
+++ src/main/java/com/tp1/core/controleur/PassagerControleur.java
@@ -1,13 +1,20 @@
 package com.tp1.core.controleur;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tp1.core.entite.PassagerEntite;
 import com.tp1.core.service.PassagerService;
+
 
 @RestController
 public class PassagerControleur {
@@ -26,5 +33,30 @@ public class PassagerControleur {
     @GetMapping("/passagers-with-passport")
     public List<PassagerEntite> afficherPassagerAvecPasseport(){
         return passagerService.getPassagersAvecPasseport();
+    }
+
+    @GetMapping("/passagers-with-idcard")
+    public List<PassagerEntite> afficherPassagersAvecPieceID() {
+        return passagerService.getPassagersAvecPieceID();
+    }
+
+    @GetMapping("/passager/{id}")
+    public Optional<PassagerEntite> afficherPassager(@PathVariable Integer id){
+        return passagerService.getPassagerParId(id);
+    }
+    
+    @PostMapping("/register-passager")
+    public void creerNouveauPassager(@RequestBody PassagerEntite p){
+        passagerService.createNewPassager(p);
+    }
+
+    @DeleteMapping("/delete-passager/{id}")
+    public void supprimerPassager(@PathVariable Integer id){
+        passagerService.deletePassager(id);
+    }
+
+    @PutMapping("/change-passager/{id}")
+    public void modifierPassager(@PathVariable Integer id, @RequestBody PassagerEntite p){
+        passagerService.modifyPassager(id, p);
     }
 }
--- src/main/java/com/tp1/core/service/PassagerService.java
+++ src/main/java/com/tp1/core/service/PassagerService.java
@@ -1,6 +1,7 @@
 package com.tp1.core.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,8 @@ public class PassagerService {
         return passagerRepository.findAll();
     }  
 
-    public PassagerEntite getPassagerParId(int id) {
-        return passagerRepository.findById(id).orElse(null);
+    public Optional<PassagerEntite> getPassagerParId(int id) {
+        return passagerRepository.findById(id);
     }
     
     public List<PassagerEntite> getPassagersAvecPasseport(){
@@ -30,4 +31,47 @@ public class PassagerService {
             .filter(p -> p.getPieceIdentite().equals("Passeport"))
             .collect(Collectors.toList());
     }
+
+    public List<PassagerEntite> getPassagersAvecPieceID(){
+        return passagerRepository
+            .findAll()
+            .stream()
+            .filter(p -> p.getPieceIdentite().equals("Carte d'identité"))
+            .collect(Collectors.toList());
+    }
+
+    public void deletePassager(Integer id) {
+        PassagerEntite p = passagerRepository.findById(id).get();
+        if(p == null) System.err.println("Cet utilisateur n'existait même pas !");
+        else{
+            passagerRepository.delete(p);
+            System.out.println("Bravo, vous avez supprimé un passager !");
+        }   
+    }
+
+
+    public void createNewPassager(PassagerEntite p){
+        passagerRepository.save(p);
+        System.out.println("Bravo, votre passager est enregistré !");
+    }
+
+    public void modifyPassager(Integer id, PassagerEntite p) {
+        Optional<PassagerEntite> recover = passagerRepository.findById(id);
+        if(recover.isPresent()){
+            PassagerEntite p1 = recover.get();
+            p1.setNom(p1.getNom());
+            p1.setPrenom(p1.getPrenom());
+            p1.setNumeroIdentite(p1.getNumeroIdentite());
+            p1.setNumeroReservation(p1.getNumeroReservation());
+            p1.setPieceIdentite(p1.getPieceIdentite());
+            p1.setCommentaires(p1.getCommentaires());
+            passagerRepository.save(p1);
+            /*
+                Bean.Utils(new, old, ignored properties...)
+            */
+            System.out.println("Votre passager à modifier ses infos personnelles");
+        }
+    }
+
+    
 }
